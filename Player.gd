84719f487e972b49982e021a5e4aaf633962c9da
Aiyana Arnobit <anzarnobit28@gmail.com>
2023-05-15 15:57:04 -0700

    added dialogue

--- Player.gd
+++ Player.gd
@@ -9,6 +9,7 @@ var hearts = 10
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
+
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 @onready var sprite : Sprite2D = $Sprite2D
