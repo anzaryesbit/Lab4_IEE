3075c5350bb4e7a2fe53e257eb73de9cbd33005c
Trenten <eternallabrynth@gmail.com>
2023-05-15 19:02:35 -0700

    Updated beavers to activate animation when hit, plus made one beaver have a choice for how far it walks

--- Enemy_Assets/beaver2.gd
+++ Enemy_Assets/beaver2.gd
@@ -1,13 +1,24 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
+const SPEED = 50.0
 const JUMP_VELOCITY = -400.0
 
 var animated = false
 
+@onready var sprite : AnimatedSprite2D = $AnimatedBeaver
+@onready var timer : Timer = $DamagedTimer
+@export var walk_dist : Vector2
+
+var start_pos
+var end_pos
+
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+func _ready():
+	start_pos = position
+	end_pos = start_pos + walk_dist
 
 
 func _physics_process(delta):
@@ -19,14 +30,38 @@ func _physics_process(delta):
 #	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 #		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-#	var direction = Input.get_axis("ui_left", "ui_right")
+	if global_position.x <= start_pos.x:
+		velocity.x = SPEED
+		sprite.flip_h = true
+	elif global_position.x >= end_pos.x:
+		velocity.x = -1 * SPEED
+		sprite.flip_h = false
+		
+	if velocity.x == 0:
+		if sprite.flip_h == true:
+			sprite.flip_h = false
+			velocity.x = -1 * SPEED
+		else:
+			sprite.flip_h = true
+			velocity.x = SPEED
+
+
 #	if direction:
 #		velocity.x = direction * SPEED
 #	else:
 #		velocity.x = move_toward(velocity.x, 0, SPEED)
 
-	move_and_slide()
+	if timer.is_stopped():
+		move_and_slide()
+		update_animation()
+	else:
+		update_animation()
+	
+
+func update_animation():
+	if timer.is_stopped():
+		sprite.play("idle")
+	else:
+		sprite.play("damaged")
 
 
