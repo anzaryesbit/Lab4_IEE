4c34fa61939d4ff0f866f5d8f6ef704a12579e4c
Aiyana Arnobit <anzarnobit28@gmail.com>
2023-05-16 23:25:44 -0700

    changed end screen if win

--- Enemy_Assets/Damageable.gd
+++ Enemy_Assets/Damageable.gd
@@ -5,12 +5,15 @@ class_name Damageable
 @export var health : int = 5
 @export var sprite : AnimatedSprite2D
 @export var timer : Timer
+@export var character : CharacterBody2D
 
 func hit(damage : int):
 	health -= damage
 	timer.start(0)
 	
 	if (health <= 0):
+		if character.name == "BossBeaver":
+			get_tree().change_scene_to_file("res://congrats.tscn")
 		get_parent().queue_free()
 
 # Called when the node enters the scene tree for the first time.
