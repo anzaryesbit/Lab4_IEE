4ecb094fc774c512f8aad9a52a0db05b475a3af8
tristandelemos <tjdelemos@gmail.com>
2023-05-16 23:03:54 -0700

    Merge branch 'main' of https://github.com/anzaryesbit/Lab4_IEE

--- Credits.gd
+++ Credits.gd
@@ -4,3 +4,7 @@ extends Control
 
 func _on_button_pressed():
 	get_tree().change_scene_to_file("res://title_screen.tscn")
+
+
+func _on_button_2_pressed():
+	get_tree().change_scene_to_file("res://sources.tscn")
---
+++ sources.gd
@@ -0,0 +1,7 @@
+extends Control
+
+
+
+
+func _on_back_2_pressed():
+	get_tree().change_scene_to_file("res://title_screen.tscn")
